ae27d6361c96c3c46c56cab0590e9d37f54f8af1
lpszeto <lpszeto@gmail.com>
2020-03-23 22:52:53 -0700

    started the test

--- app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
+++ app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
@@ -8,29 +8,37 @@ public class TTTStateTest {
 
     @Test
     public void getPiece() {
+        TTTState tempState = new TTTState();
+
     }
 
     @Test
     public void setPiece() {
+        TTTState tempState = new TTTState();
     }
 
     @Test
     public void getWhoseMove() {
+        TTTState tempState = new TTTState();
     }
 
     @Test
     public void setWhoseMove() {
+        TTTState tempState = new TTTState();
     }
 
     @Test
     public void getPosition() {
+        TTTState tempState = new TTTState();
     }
 
     @Test
     public void getWord() {
+        TTTState tempState = new TTTState();
     }
 
     @Test
     public void shuffle() {
+        TTTState tempState = new TTTState();
     }
 }
